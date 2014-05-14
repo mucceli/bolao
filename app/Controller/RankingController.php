8177abe75494a0fe5d1a0a6aa91aa4b75ab22703
Frederico Mucceli <mucceli@gmail.com>
2014-05-14 04:08:42 -0300

    UC Ranking

--- app/Controller/RankingController.php
+++ app/Controller/RankingController.php
@@ -3,48 +3,36 @@
  * @author mucceli
  */
 class RankingController extends AppController {
-        
-     /**
-     * Método necessário para adequar as convenções do Cake. Nome do file é  "painel_cadastrar_Equipe.ctp"
-     */
-    public function index() {
-        // $equipes = $this->paginate('Equipe');
-        // $this->set('equipes', $equipes);  
-    }
-
-    public function cadastrar_equipe(){
-         if (!empty($this->data)) {
-            // se os dados do formulário puderam ser validados e salvos...
-            if ($this->Equipe->save($this->data)) {
-                // define uma mensagem de flash na sessão e redireciona.
-                $this->Session->setFlash("Equipe salva com sucesso!", "default", array('class' => ''));
-                $this->redirect(array('controller' => 'Equipes','action' => 'index'));
-            }
-        }
-    }
-
-    function excluir_equipe($id) {
-        if (!empty($id)):
-            if ($this->Equipe->delete($id,true)):
-                $this->Session->setFlash("Equipe excluída com sucesso!","default", array('class' => 'alert alert-success'));
-            else:
-                $this->Session->setFlash("Não foi possível excluir a equipe!","default", array('class' => 'alert alert-error'));
-            endif;
-            $this->redirect(array('controller' => 'Equipes','action' => 'index'));
-        endif;
-    }
     
-    public function editar_equipe($id) {
-       if (!empty($this->data)) {
-           $this->Equipe->id = $this->Session->read('id_Equipe_edit');
-           $this->Session->delete('id_Equipe_edit');
-            // se os dados do formulário puderam ser validados e salvos...
-            if ($this->Equipe->save($this->data)) {
-                // define uma mensagem de flash na sessão e redireciona.
-                $this->Session->setFlash("Equipe alterada com sucesso!", "default", array('class' => 'alert alert-success'));
-                $this->redirect(array('controller' => 'Equipes','action' => 'index'));
+    var $uses = array('Aposta','User'); 
+
+    public function index() {
+        $users = $this->User->find('all');
+        $apostas = $this->Aposta->find('all',array('recursive' => 2));
+        $ranking = array();        
+        foreach ($users as $user) {
+            $usuario = $user["User"]["username"];
+            $total_pontos = 0;
+            foreach ($apostas as $aposta) {
+                if($aposta["Aposta"]["user_id"] == $user["User"]["id"]){
+                    if(!empty($aposta["Aposta"]["pontos"])){
+                        $total_pontos += $aposta["Aposta"]["pontos"];
+                    }
+                }
             }
+            $ranking[$usuario] = $total_pontos;
         }
+
+        arsort($ranking);
+        $this->set('ranking',$ranking);
+        $this->set('users',$users);
+    }
+
+    public function atualizar_pontos() {
+        $jogos = $this->Jogo->find('all');
+        $apostas = $this->Aposta->find('all',array('recursive' => 2));
+        $this->set('apostas',$apostas);
+        $this->redirect(array('controller' => 'Ranking','action' => 'index'));
     }
    
 }
--- app/View/Ranking/index.ctp
+++ app/View/Ranking/index.ctp
@@ -3,5 +3,37 @@
 </head>
 <body>
 Ranking
+<br>
+<br>
+<?php echo $this->Session->flash(); ?>
+	    <table class="table table-bordered table-striped table-hover">
+	        <tr>
+	            <th>Colocação</th>
+	            <th>Nome</th>
+	            <th>Pontos</th>
+	        </tr>
+	        <?php
+	        $i =1;
+	        foreach ($ranking as $key => $value):
+	            ?>
+						<tr> 
+			            	<td>
+			            		<?php echo $i ?>º
+			            	</td>
+	            			<td>
+		                        <?php echo $key ?>
+	                        </td>             
+			                <td style="width: 35px;">
+			                	<?php pr($value)?>
+			                </td>
+			            </tr>
+	                <?php $i++;  ?>
+	         <?php
+	        endforeach; ?>
+	    </table>
+	    <br>
+<form action="<?php echo $this->webroot.'ranking/atualizar_pontos'?>" method="POST">
+		<input type="submit" class="bt bt-v btapostar" value="Atualizar Ranking"/>
+</form> 
 </body>
 </html>
